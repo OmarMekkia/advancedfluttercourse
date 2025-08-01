466b9c9d167cdb02b60ba447a6d871e3578b3caa
OmarMekkia <omarmakkia5@gmail.com>
2025-08-01 16:59:56 +0300

    just a fake push to origin development

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:advancedfluttercourse/core/routing/app_router.dart';
 import 'package:advancedfluttercourse/doc_app.dart';
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp( DocApp(appRouter: AppRouter.router));
 }
